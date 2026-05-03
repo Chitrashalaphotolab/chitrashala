ee92717ea3ca829bab741d2209356a03ded77153
YVenkatateja <venkatateja310@gmail.com>
2026-05-03 18:15:51 +0530

    Fix router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/chitrashala">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/gallery" element={<GalleryPage />} />
